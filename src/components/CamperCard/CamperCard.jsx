6cc7652438cc665d364f85fd347c5e11324581c3
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-09-01 23:32:08 +0200

    photo added

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -4,9 +4,10 @@ import Feature from "../Feature/Feature";
 export default function CamperCard({ camper }) {
     const camperFeatures = Object.entries(camper).filter((entry)=> entry.includes(true))
     const featureList = camperFeatures.map((entry, index)=><li key={index}> <Feature name ={entry[0]}/></li>);
+    const [{thumb}] = camper.gallery
   return (
     <div>
-      <img></img>
+      <img src={thumb}></img>
       <div>
         <p>{camper.name}</p>
         <p>{camper.price}</p>
